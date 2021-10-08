0304ce3980e20e608424b200588c2938144bc3e0
Zahra <zahra.khan@blockfi.com>
2021-10-08 15:06:32 -0600

    [CLEAN UP] Remove unused files

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,11 @@
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
+      <h1>Hello World!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
